7b02ed053d502a519cbe03db3dcd2eb868bef310
Pearl Christian <pearl@cyvyon.com>
2025-12-16 23:57:58 +0530

    added dashboard

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,10 +1,8 @@
-import { Button } from "@/components/ui/button"
+import Page from "./app/dashboard/page"
 
 function App() {
   return (
-    <div className="flex min-h-svh flex-col items-center justify-center">
-      <Button>Click me</Button>
-    </div>
+    <Page/>
   )
 }
 
