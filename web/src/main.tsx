954c5238839a1d48e22e6495814cac2094cd7e83
swapnil404 <swapnil.christian.developer@gmail.com>
2026-01-06 16:44:49 +0530

    tansatck query2

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -2,15 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
-import {
-  useQuery,
-  useMutation,
-  useQueryClient,
-  QueryClient,
-  QueryClientProvider,
-} from "@tanstack/react-query";
 
-const queryClient = new QueryClient();
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
