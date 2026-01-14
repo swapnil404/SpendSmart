bbf6bd418be3162fa1e1149133fd794646744a36
swapnil404 <swapnil.christian.developer@gmail.com>
2026-01-14 16:51:33 +0530

    idk bruh

--- server/src/db/db.ts
+++ server/src/db/db.ts
@@ -1,11 +1,8 @@
 import { drizzle } from "drizzle-orm/neon-http";
 import { neon } from "@neondatabase/serverless";
+import { config } from "dotenv";
 
-const databaseUrl = process.env.DATABASE_URL;
+config({ path: ".env" }); // or .env.local
 
-if (!databaseUrl) {
-  throw new Error("DATABASE_URL is not set!");
-}
-
-const sql = neon(databaseUrl);
-export const db = drizzle(sql);
+const sql = neon(process.env.DATABASE_URL!);
+export const db = drizzle({ client: sql });
